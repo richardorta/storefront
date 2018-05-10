8caecca5879b88f1a16480fb2fca3a759ef84f13
Richard <richard@wind.HomeGroup>
2018-05-09 20:38:34 -0500

    Created simple user domain file.

---
+++ grails-app/domain/storefront/Account/user.groovy
@@ -0,0 +1,9 @@
+package storefront.account
+
+class user {
+    String      acct
+    String      fullName
+    
+    static constraints = {
+    }
+}
---
+++ src/test/groovy/storefront/Account/userSpec.groovy
@@ -0,0 +1,18 @@
+package storefront
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class Account/userSpec extends Specification implements DomainUnitTest<Account/user> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
