2a518ec7827deb2b28df32330a1d38e03f744820
richardorta <37092035+richardorta@users.noreply.github.com>
2018-05-12 21:15:45 -0500

    Added columns to User.

--- grails-app/domain/account/User.groovy
+++ grails-app/domain/account/User.groovy
@@ -3,7 +3,10 @@ package account
 class User {
     String userAccount
     String userFullname
+    String email
+    Date createdOn
 
     static constraints = {
+        userAccount blank: false, maxSize: 32
     }
 }
