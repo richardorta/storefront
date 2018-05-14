f485461af6fcaced0caccd0a67db59979ba9f0ac
Richard Orta <richard@richards-mbp.attlocal.net>
2018-05-13 20:44:54 -0500

    Test change.

--- src/test/groovy/account/UserSpec.groovy
+++ src/test/groovy/account/UserSpec.groovy
@@ -1,31 +1,28 @@
 package account
 
+import grails.testing.mixin.integration.Integration
+import grails.gorm.transactions.*
 import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 
+@Integration
+@Rollback
 class UserSpec extends Specification implements DomainUnitTest<User> {
 
-    def setup() {
-        new User(userAccount: 'jdoe').save(flush: true)
+    void setup() {
+        new User(userAccount: 'jdoe', userFullname:'John Doe').save(flush: true)
     }
 
     def cleanup() {
     }
 
-    void "testCreateAccount"() {
-//        given:
+    void "test - add user"() {
+        given:
 //            setup()
 
         expect:
             User.count() == 1
     }
 
-//    void "test"() {
-//        given:
-//            setup()
-//
-//        expect:
-//            userAccount == 'jdoe'
-//    }
 
 }
